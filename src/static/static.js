65cdbee6b5e8de75cf14a69e786022c88677e6ee
Ibrokhimweb <ibrokhimgithub@gmail.com>
2022-09-13 20:04:15 +0500

    static file added new date

--- src/static/static.js
+++ src/static/static.js
@@ -7,4 +7,23 @@ export const users = [
   {}
 ];
 
+export const heartedPosts = [
+  {img: 'link', title: 'nomi'},
+  {img: 'link', title: 'nomi'},
+  {img: 'link', title: 'nomi'},
+  {img: 'link', title: 'nomi'}
+]
 
+
+export const recommendedPosts = [
+  {
+    id: 1,
+    type: 'video',
+    url: 'link'
+  },
+  {
+    id: 2,
+    type: 'image',
+    url: 'link'
+  },
+];
