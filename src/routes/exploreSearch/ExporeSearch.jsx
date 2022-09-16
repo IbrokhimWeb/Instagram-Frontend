81e32cc10c40891a21a69e5d58d1ded87ef6af24
RamazonovDeveloper <ramazonovferuz01@gmail.com>
2022-09-16 10:20:08 +0500

    Created recommended page

--- src/routes/exploreSearch/ExporeSearch.jsx
+++ src/routes/exploreSearch/ExporeSearch.jsx
@@ -1,9 +1,35 @@
 // @ts-nocheck
 import React from "react";
-import s from "./ExploreSearch.module.css";
+import { recommendedPosts } from "../../static/static";
+import s from "./ExploreSearch.module.scss";
+
+import {BsPlayFill} from 'react-icons/bs'
+import {FcLike} from 'react-icons/fc'
 
 const ExporeSearch = () => {
-  return <div>Expore</div>;
+  return <div className={s.container}>
+    {recommendedPosts.map((index, data) => {
+        if(recommendedPosts[data].type == 'foto'){
+          return <div key={index} onMouseEnter={()=>{}} className={s.item}>
+            <img className="" src={recommendedPosts[data].url.foto} alt="" />
+            <div className={s.hover}>
+              <FcLike className={s.like}/>
+              100k
+            </div>
+        </div>
+        }
+        else{
+          return <div key={index} className={s.item}>
+            <BsPlayFill className={s.icon}/>
+            <video className="" src={recommendedPosts[data].url.video}/>
+            <div className={s.hover}>
+              <FcLike className={s.like}/>
+              100k
+            </div>
+          </div>
+        }
+      })}
+  </div>;
 };
 
 export default ExporeSearch;
--- src/static/static.js
+++ src/static/static.js
@@ -1,4 +1,9 @@
 // @ts-nocheck
+
+import foto from '../assets/foto1.jpg'
+import video from '../assets/video.MP4'
+
+
 export const posts = [
   {}
 ];
@@ -18,12 +23,42 @@ export const heartedPosts = [
 export const recommendedPosts = [
   {
     id: 1,
-    type: 'video',
-    url: 'link'
+    type: 'foto',
+    url: {foto}
   },
   {
     id: 2,
-    type: 'image',
-    url: 'link'
+    type: 'video',
+    url: {video}
+  },
+  {
+    id: 3,
+    type: 'foto',
+    url: {foto}
+  },
+  {
+    id: 4,
+    type: 'video',
+    url: {video}
+  },
+  {
+    id: 5,
+    type: 'foto',
+    url: {foto}
+  },
+  {
+    id: 6,
+    type: 'video',
+    url: {video}
+  },
+  {
+    id: 7,
+    type: 'foto',
+    url: {foto}
+  },
+  {
+    id: 8,
+    type: 'video',
+    url: {video}
   },
 ];
