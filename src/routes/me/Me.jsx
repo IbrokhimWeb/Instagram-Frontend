687e8b8683eb51c50eed911c80f5a006b2fb3b90
Mustafo Yakubov <mustafa6143170@gmail.com>
2022-09-18 18:24:11 +0500

    Route - Me Header done

--- src/routes/me/Me.jsx
+++ src/routes/me/Me.jsx
@@ -1,11 +1,23 @@
 // @ts-nocheck
-import React from 'react';
+import React from "react";
 import s from "./Me.module.css";
-
+import { users } from "../../static/static";
 const Me = () => {
   return (
-    <div>Me</div>
-  )
-}
+    <>
+      <div className={s.container}>
+        <div className={s.userProfile}>
+          {users.map((user, id) => {
+            return (
+              <div key={id} className={s.userProfileHeader}>
+                <img className={s.userAvatar} src={user.avatar} alt={user.username} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Me
+export default Me;
--- src/static/static.js
+++ src/static/static.js
@@ -1,29 +1,40 @@
 // @ts-nocheck
-export const posts = [
-  {}
-];
+// import static image
+import userAvatar from "../assets/userAvatar.png";
+export const posts = [{}];
 
 export const users = [
-  {}
+  {
+    id: "1234asfdgf",
+    avatar: userAvatar,
+    username: "zokirkhon1002",
+    password: "12345678",
+    firstName: "Zokirkhon",
+    lastName: "Kotibkhonov",
+    bio: "Work Hard Dream Big",
+    desc: "Student in Chonnam National University",
+    followers: [],
+    following: [],
+    postLikes: [],
+    savedPosts: [],
+  },
 ];
-
 export const heartedPosts = [
-  {img: 'link', title: 'nomi'},
-  {img: 'link', title: 'nomi'},
-  {img: 'link', title: 'nomi'},
-  {img: 'link', title: 'nomi'}
-]
-
+  { img: "link", title: "nomi" },
+  { img: "link", title: "nomi" },
+  { img: "link", title: "nomi" },
+  { img: "link", title: "nomi" },
+];
 
 export const recommendedPosts = [
   {
     id: 1,
-    type: 'video',
-    url: 'link'
+    type: "video",
+    url: "link",
   },
   {
     id: 2,
-    type: 'image',
-    url: 'link'
+    type: "image",
+    url: "link",
   },
 ];
